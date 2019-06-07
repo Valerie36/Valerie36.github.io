0b178dcf480323a4e46febbad37f1e0bf97c2a7e
Valerie <m26416079@john.petra.ac.id>
2019-06-07 16:44:45 +0700

    Fix

--- index.php
+++ index.php
@@ -8,13 +8,10 @@
 	<link rel="manifest" href="manifest.json">
 	<title>Meal Search</title>
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
-	<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-	<script src="jquery-3.4.1.slim.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="bootstrap/js/bootstrap.min.js"></script>
-	<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
-	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script> -->
-	<script src="jquery-3.4.1.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script type="text/javascript">
 		if ('serviceWorker' in navigator) { 
 			window.addEventListener('load', () => { 
@@ -87,28 +84,6 @@
      	}
      	.flex > div {
      		flex-grow: 0;
-     	}
-     }
-
-     @media only screen and (max-width: 480px) {
-     	body{
-     		font-size: 10px;
-     	}
-     	.flex{
-     		flex-direction: column;
-     	}
-     	.flex > div {
-     		flex-grow: 0;
-     	}
-     	button {
-     		width: 	80px;
-     	}
-     	.btn{
-     		width: 	80px;
-     		font-size: 	10px;
-     	}
-     	.btn-danger{
-     		width: 20px;
      	}
      }
      ul {
@@ -188,8 +163,8 @@
 	</div>
 
 	<div class="modal fade" id="FavoriteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" >
-		<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-			<div class="modal-content" style="font-size:-5vw;">
+		<div class="modal-dialog modal-dialog-centered modal-lg" role="document" style="width: 800px;">
+			<div class="modal-content" style="width: 800px;">
 				<div class="modal-header">
 					<h5 class="modal-title">Favorite !</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -200,10 +175,10 @@
 					<table class="table">
 						<thead>
 							<tr>
-								<th scope="col">Title</th>
-								<th scope="col">Recipe</th>
-								<th scope="col">Instruct</th>
-								<th scope="col">Del</th>
+								<th scope="col"style="width: 100px" >Title</th>
+								<th scope="col" style="width: 200px;">Recipe</th>
+								<th scope="col">Instruction</th>
+								<th scope="col">Delete</th>
 							</tr>
 						</thead>
 						<tbody id="favlist" class="favlist">
@@ -313,7 +288,7 @@
 				var cursor = event.target.result;
 
 				if (cursor) {
-					document.getElementById("favlist").innerHTML+=" <tr><td>"+cursor.value.title+"</td><td>"+cursor.value.recipe+"</td><td>"+cursor.value.instruct+"</td><td><button id='"+cursor.key+"' class='btn btn-danger' onclick='remove(this.id)' data-dismiss='modal' id='Remove'>X</button></td></tr>"
+					document.getElementById("favlist").innerHTML+=" <tr><td>"+cursor.value.title+"</td><td>"+cursor.value.recipe+"</td><td>"+cursor.value.instruct+"</td><td><button id='"+cursor.key+"' class='btn btn-danger' onclick='remove(this.id)'data-dismiss='modal'>Remove</button></td></tr>"
 					cursor.continue();
 				} else {
 					console.log("No entry");
@@ -327,7 +302,7 @@
 			.delete(Number(id));
 
 			request.onsuccess = function(event) {
-				alert("Item has been remove to your database.");
+				alert("Item has been added to your database.");
 			};
 		}
 
