26ac851833427b90c8982baaf88ef51b7e54c301
Valerie <m26416079@john.petra.ac.id>
2019-06-07 17:08:57 +0700

    Fix3

--- service-worker.js
+++ service-worker.js
@@ -36,9 +36,17 @@ self.addEventListener('install', (evt) => {
 	evt.waitUntil( 
 		caches.open(CACHE_NAME).then((cache) => { 
 			console.log('[ServiceWorker] Pre-caching offline page'); 
-			return cache.addAll(FILES_TO_CACHE);
+			return cache.addAll(
+				[
+					'/offline.html',
+					'/jquery-3.4.1.min.js',
+					'/jquery-3.4.1.slim.min.js',
+					'/bootstrap/js/bootstrap.min.js',
+					'/bootstrap/css/bootstrap.min.css'
+				]
+			);
 		}) 
-		);
+	);
 	self.skipWaiting();
 
 });
