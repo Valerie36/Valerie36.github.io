91c9605a4daa09918b0eb6a6f41070d1b5050531
Valerie <m26416079@john.petra.ac.id>
2019-06-19 01:06:14 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,62 @@
-const FILES_TO_CACHE = [ 'offline.html', ];
-var CACHE_NAME = 'Cache'; 
+var CACHE_NAME = 'meal-cache-v1';
+var CACHE_DYNAMIC_NAME = 'meal-cache-v2';
 
-self.addEventListener('fetch', function(evt) {
-	if (evt.request.mode !== 'navigate') 
-{ 
-	// Not a page navigation, bail. return; 
-} 
-evt.respondWith( 
-	fetch(evt.request) 
-	.catch(() => { 
-		return caches.open(CACHE_NAME) 
-		.then((cache) => { 
-			return cache.match('offline.html'); 
-		}); 
-	}) 
-	); 
-
+self.addEventListener('install', function(event) {
+	console.log('[Service Worker] Installing Service Worker ...', event);
+	event.waitUntil(
+		caches.open(CACHE_DYNAMIC_NAME)
+		.then(function(cache) {
+			console.log('[Service Worker] Precaching App Shell');
+			cache.addAll([
+				'/',
+				'/offline.html',
+				'/jquery-3.4.1.min.js',
+				'/jquery-3.4.1.slim.min.js',
+				'/bootstrap/js/bootstrap.min.js',
+				'/bootstrap/css/bootstrap.min.css'
+				]);
+		})
+		)
 });
 
 self.addEventListener('activate', (evt) => {
 	evt.waitUntil( 
-		caches.keys().then((keyList) => { 
+		caches.keys()
+		.then((keyList) => { 
 			return Promise.all(keyList.map((key) => { 
-				if (key !== CACHE_NAME) { 
+				if (key !== CACHE_NAME && key !== CACHE_DYNAMIC_NAME) { 
 					console.log('[ServiceWorker] Removing old cache', key); 
 					return caches.delete(key); 
 				} 
 			}));
 		})
-		); 
-
+		);
+	return self.clients.claim();
 });
 
-self.addEventListener('install', (evt) => {
-	evt.waitUntil( 
-		caches.open(CACHE_NAME).then((cache) => { 
-			console.log('[ServiceWorker] Pre-caching offline page'); 
-			return cache.addAll(
-				[
-					'/offline.html',
-					'/jquery-3.4.1.min.js',
-					'/jquery-3.4.1.slim.min.js',
-					'/bootstrap/js/bootstrap.min.js',
-					'/bootstrap/css/bootstrap.min.css'
-				]
-			);
-		}) 
-	);
-	self.skipWaiting();
-
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			if (response) {
+				return response;
+			} else {
+				return fetch(event.request)
+				.then(function(res) {
+					return caches.open(CACHE_DYNAMIC_NAME)
+					.then(function(cache) {
+						cache.put(event.request.url, res.clone());
+						return res;
+					})
+				})
+				.catch(function(err) {
+					return caches.open(CACHE_NAME)
+					.then(function(cache) {
+						return cache.match('offline.html');
+					});
+				});
+			}
+		})
+		);
 });
+
